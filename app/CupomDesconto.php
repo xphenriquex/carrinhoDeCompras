8a9d7b5aa0aa104ee5296f3b959020678162a056
Paulo Henrique <desenvolvedores@sauderesidence.com.br>
2018-11-21 16:15:14 -0300

    Criando fillable dos models

--- app/CupomDesconto.php
+++ app/CupomDesconto.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class CupomDesconto extends Model
 {
-    //
+    protected $fillable = [
+        'nome',
+        'localizador',
+        'desconto',
+        'modo_desconto',
+        'limite',
+        'modo_limite',
+        'dthr_validade',
+        'ativo'
+    ];
 }
--- app/Produto.php
+++ app/Produto.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    protected $fillable = [
+        'nome',
+        'descricao',
+        'imagem',
+        'valor',
+        'ativo'
+    ];
 }
