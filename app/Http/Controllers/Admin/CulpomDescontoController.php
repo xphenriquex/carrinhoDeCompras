85ec9a54bcc2861b644ae707ac7e1a0d58eac431
Paulo Henrique <desenvolvedores@sauderesidence.com.br>
2018-11-21 16:12:23 -0300

    Implementando metodo de controllers

--- app/Http/Controllers/Admin/CulpomDescontoController.php
+++ app/Http/Controllers/Admin/CulpomDescontoController.php
@@ -5,81 +5,59 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class CulpomDescontoController extends Controller
+use App\CupomDesconto;
+
+class CupomDescontoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $cupons = CupomDesconto::all();
+        return view('admin.cupom_desconto.index', compact('cupons'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function adicionar()
     {
-        //
+        return view('admin.cupom_desconto.adicionar');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function editar($id)
     {
-        //
+        $registro = CupomDesconto::find($id);
+        if( empty($registro->id) ) {
+            return redirect()->route('admin.cupons');
+        }
+        return view('admin.cupom_desconto.editar', compact('registro'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function salvar(Request $req)
     {
-        //
+        $dados = $req->all();
+
+        CupomDesconto::create($dados);
+
+        $req->session()->flash('admin-mensagem-sucesso', 'Cupom de desconto criado com sucesso!');
+
+        return redirect()->route('admin.cupons');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function atualizar(Request $req, $id)
     {
-        //
+        $dados = $req->all();
+
+        CupomDesconto::find($id)->update($dados);
+
+        $req->session()->flash('admin-mensagem-sucesso', 'Cupom de desconto atualizado com sucesso!');
+
+        return redirect()->route('admin.cupons');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function deletar($id)
     {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        CupomDesconto::find($id)->delete();
+
+        $req->session()->flash('admin-mensagem-sucesso', 'Cupom de desconto deletado com sucesso!');
+
+        return redirect()->route('admin.cupons');
     }
 }
--- app/Http/Controllers/Admin/ProdutoController.php
+++ app/Http/Controllers/Admin/ProdutoController.php
@@ -1,85 +1,63 @@
 <?php
 
-namespace App\Http\Controllers\admin;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+
 use App\Http\Controllers\Controller;
+use App\Produto;
 
 class ProdutoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $produtos = Produto::all();
+        return view('admin.produto.index', compact('produtos'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function adicionar()
     {
-        //
+        return view('admin.produto.adicionar');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function editar($id)
     {
-        //
+        $registro = Produto::find($id);
+        if( empty($registro->id) ) {
+            return redirect()->route('admin.produtos');
+        }
+        return view('admin.produto.editar', compact('registro'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function salvar(Request $req)
     {
-        //
+        $dados = $req->all();
+
+        Produto::create($dados);
+
+        $req->session()->flash('admin-mensagem-sucesso', 'Produto cadastrado com sucesso!');
+
+        return redirect()->route('admin.produtos');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function atualizar(Request $req, $id)
     {
-        //
+        $dados = $req->all();
+
+        Produto::find($id)->update($dados);
+
+        $req->session()->flash('admin-mensagem-sucesso', 'Produto atualizado com sucesso!');
+
+        return redirect()->route('admin.produtos');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function deletar($id)
     {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        Produto::find($id)->delete();
+
+        $req->session()->flash('admin-mensagem-sucesso', 'Produto deletado com sucesso!');
+
+        return redirect()->route('admin.produtos');
     }
 }
